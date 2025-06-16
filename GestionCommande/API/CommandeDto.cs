3e883d1fa960a4a970cf37d11631b465074f8f13
Said WAHID <said@teknologiate.com>
2025-06-16 14:56:48 +0100

    Creation projet API

---
+++ GestionCommande/API/CommandeDto.cs
@@ -0,0 +1,13 @@
+﻿namespace API
+{
+    public class CommandeDto
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int NCommande { get; set; }
+        public Decimal Montant { get; set; }
+        public string? Client { get; set; }
+        public bool EstLivree { get; set; }
+
+    }
+}
--- GestionCommande/API/Program.cs
+++ GestionCommande/API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,31 +16,24 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
+app.MapGet("/", () => "Hello World!");
+app.MapPost("/api/echo", (string message) =>
+{
+    if (string.IsNullOrEmpty(message))
+    {
+        return Results.BadRequest(new { Error = "Message cannot be empty." });
+    }
+    else
+    {
+        return Results.Ok(new { Message = message });
+
+    }
+});
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
